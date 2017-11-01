15aaba1c4ae2385cf24f1b853196b6dc69a46989
Juan Esteban Echavarria <juan.echavarria@techandsolve.com>
2017-11-01 12:13:18 -0500

    Premium: Noticias

--- resources/views/premium.blade.php
+++ resources/views/premium.blade.php
@@ -85,12 +85,10 @@
                         <div class="panel panel-headline">
                             <div class="panel-heading">
                                 <h3 class="panel-title">Noticias</h3>
-                                <p class="panel-subtitle"><iframe src="//rss.bloople.net/?url=http%3A%2F%2Fnutricionaldia.fullblog.com.ar%2F%3Frss%3D1&detail=50&showtitle=false&type=html"></iframe></p>
+                                <p class="panel-subtitle"></p>
                             </div>
                             <div class="panel-body">
-                                <h4>Panel Content</h4>
-                                <p>Objectively network visionary methodologies via best-of-breed users. Phosfluorescently initiate go forward leadership skills before an expanded array of infomediaries. Monotonectally incubate web-enabled communities rather than process-centric.</p>
-                            </div>
+                                <iframe src="//rss.bloople.net/?url=http%3A%2F%2Fnutricionaldia.fullblog.com.ar%2F%3Frss%3D1&detail=50&showtitle=false&type=html"></iframe>                            </div>
                         </div>
                         <!-- END PANEL HEADLINE -->
                     </div>
