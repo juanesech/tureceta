ac3df66e8ca3e53e0c3ef2163656e7a168196631
Juan Esteban Echavarria <juan.echavarria@techandsolve.com>
2017-11-03 13:54:11 -0500

    Premium: videos

--- resources/views/premium.blade.php
+++ resources/views/premium.blade.php
@@ -17,6 +17,12 @@
     <!-- ICONS -->
     <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
     <link rel="icon" type="image/png" sizes="96x96" href="assets/img/favicon.png">
+    <!-- Video -->
+    <script src="assets/scripts/jquery.js"></script>
+    <script src="assets/scripts/amazingslider.js"></script>
+    <link rel="stylesheet" type="text/css" href="assets/scripts/amazingslider-1.css">
+    <script src="assets/scripts/initslider-1.js"></script>
+
 </head>
 
 <body>
@@ -126,10 +132,31 @@
                             <!-- PANEL NO CONTROLS -->
                             <div class="panel">
                                 <div class="panel-heading">
-                                    <h3 class="panel-title">Panel No Right Controls</h3>
+                                    <h3 class="panel-title">Video Tutoriales</h3>
                                 </div>
                                 <div class="panel-body">
-                                    <p>Objectively network visionary methodologies via best-of-breed users. Phosfluorescently initiate go forward leadership skills before an expanded array of infomediaries. Monotonectally incubate web-enabled communities rather than process-centric.</p>
+                                    <div id="amazingslider-wrapper-1" style="display:block;position:relative;max-width:900px;margin:0px auto 0px;">
+                                        <div id="amazingslider-1" style="display:block;position:relative;margin:0 auto;">
+                                            <ul class="amazingslider-slides" style="display:none;">
+                                                <li><img src="assets/img/pGx9w2GJ_mc.jpg" alt="Pollo"  title="Pollo" />
+                                                    <video preload="none" src="https://www.youtube.com/embed/pGx9w2GJ_mc?v=pGx9w2GJ_mc" ></video>
+                                                </li>
+                                                <li><img src="assets/img/OeCUsncUPfo.jpg" alt="Ratatouille"  title="Ratatouille" data-description="Si, el de la pelicula" />
+                                                    <video preload="none" src="https://www.youtube.com/embed/OeCUsncUPfo?v=OeCUsncUPfo" ></video>
+                                                </li>
+                                                <li><img src="assets/img/00yPwY6a__A.jpg" alt="Rollo de carne"  title="Rollo de carne" data-description="Rico rollo de carne" />
+                                                    <video preload="none" src="https://www.youtube.com/embed/00yPwY6a__A?v=00yPwY6a__A" ></video>
+                                                </li>
+                                                <li><img src="assets/img/XJRVrJS0xMg.jpg" alt="Banderines de salchicha"  title="Banderines de salchicha" data-description="Deliciosos banderines de salchicha y queso" />
+                                                    <video preload="none" src="https://www.youtube.com/embed/XJRVrJS0xMg?v=XJRVrJS0xMg" ></video>
+                                                </li>
+                                                <li><img src="assets/img/rUs6uLZ_4KM.jpg" alt="Aros de cebolla"  title="Aros de cebolla" data-description="Aros de cebolla super crujientes" />
+                                                    <video preload="none" src="https://www.youtube.com/embed/rUs6uLZ_4KM?v=rUs6uLZ_4KM" ></video>
+                                                </li>
+                                            </ul>
+                                            <div class="amazingslider-engine"><a href="http://amazingslider.com" title="jQuery Slideshow">jQuery Slideshow</a></div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
