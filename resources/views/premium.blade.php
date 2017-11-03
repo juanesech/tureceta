46d6c6239be55bbca3185278d71fd7b7394d7e78
Juan Esteban Echavarria <juan.echavarria@techandsolve.com>
2017-11-03 17:10:26 -0500

    Premium: videos

--- resources/views/premium.blade.php
+++ resources/views/premium.blade.php
@@ -31,7 +31,7 @@
     <!-- NAVBAR -->
     <nav class="navbar navbar-default navbar-fixed-top">
         <div class="brand">
-            <a href="index.html"><img src="assets/img/letras.png" alt="TuReceta Logo" class="img-responsive logo"></a>
+            <a href="/home"><img src="assets/img/letras.png" alt="TuReceta Logo" class="img-responsive logo"></a>
         </div>
         <div class="container-fluid">
             <div class="navbar-btn">
@@ -127,57 +127,59 @@
                     </div>
                 </div>
                 <div class="row">
-                        <!-- PANEL DEFAULT -->
-                        <div class="col-md-4" id="tutorials">
-                            <!-- PANEL NO CONTROLS -->
-                            <div class="panel">
-                                <div class="panel-heading">
-                                    <h3 class="panel-title">Video Tutoriales</h3>
-                                </div>
-                                <div class="panel-body">
-                                    <div id="amazingslider-wrapper-1" style="display:block;position:relative;max-width:900px;margin:0px auto 0px;">
-                                        <div id="amazingslider-1" style="display:block;position:relative;margin:0 auto;">
-                                            <ul class="amazingslider-slides" style="display:none;">
-                                                <li><img src="assets/img/pGx9w2GJ_mc.jpg" alt="Pollo"  title="Pollo" />
-                                                    <video preload="none" src="https://www.youtube.com/embed/pGx9w2GJ_mc?v=pGx9w2GJ_mc" ></video>
-                                                </li>
-                                                <li><img src="assets/img/OeCUsncUPfo.jpg" alt="Ratatouille"  title="Ratatouille" data-description="Si, el de la pelicula" />
-                                                    <video preload="none" src="https://www.youtube.com/embed/OeCUsncUPfo?v=OeCUsncUPfo" ></video>
-                                                </li>
-                                                <li><img src="assets/img/00yPwY6a__A.jpg" alt="Rollo de carne"  title="Rollo de carne" data-description="Rico rollo de carne" />
-                                                    <video preload="none" src="https://www.youtube.com/embed/00yPwY6a__A?v=00yPwY6a__A" ></video>
-                                                </li>
-                                                <li><img src="assets/img/XJRVrJS0xMg.jpg" alt="Banderines de salchicha"  title="Banderines de salchicha" data-description="Deliciosos banderines de salchicha y queso" />
-                                                    <video preload="none" src="https://www.youtube.com/embed/XJRVrJS0xMg?v=XJRVrJS0xMg" ></video>
-                                                </li>
-                                                <li><img src="assets/img/rUs6uLZ_4KM.jpg" alt="Aros de cebolla"  title="Aros de cebolla" data-description="Aros de cebolla super crujientes" />
-                                                    <video preload="none" src="https://www.youtube.com/embed/rUs6uLZ_4KM?v=rUs6uLZ_4KM" ></video>
-                                                </li>
-                                            </ul>
-                                            <div class="amazingslider-engine"><a href="http://amazingslider.com" title="jQuery Slideshow">jQuery Slideshow</a></div>
-                                        </div>
+                    <!-- PANEL DEFAULT -->
+                    <div class="col-md-4" id="tutorials">
+                        <!-- PANEL NO CONTROLS -->
+                        <div class="panel">
+                            <div class="panel-heading">
+                                <h3 class="panel-title">Video Tutoriales</h3>
+                            </div>
+                            <div class="panel-body">
+                                <div id="amazingslider-wrapper-1" style="display:block;position:relative;max-width:900px;margin:0px auto 0px;">
+                                    <div id="amazingslider-1" style="display:block;position:relative;margin:0 auto;">
+                                        <ul class="amazingslider-slides" style="display:none;">
+                                            <li><img src="assets/img/pGx9w2GJ_mc.jpg" alt="Pollo"  title="Pollo" />
+                                                <video preload="none" src="https://www.youtube.com/embed/pGx9w2GJ_mc?v=pGx9w2GJ_mc" ></video>
+                                            </li>
+                                            <li><img src="assets/img/OeCUsncUPfo.jpg" alt="Ratatouille"  title="Ratatouille" data-description="Si, el de la pelicula" />
+                                                <video preload="none" src="https://www.youtube.com/embed/OeCUsncUPfo?v=OeCUsncUPfo" ></video>
+                                            </li>
+                                            <li><img src="assets/img/00yPwY6a__A.jpg" alt="Rollo de carne"  title="Rollo de carne" data-description="Rico rollo de carne" />
+                                                <video preload="none" src="https://www.youtube.com/embed/00yPwY6a__A?v=00yPwY6a__A" ></video>
+                                            </li>
+                                            <li><img src="assets/img/XJRVrJS0xMg.jpg" alt="Banderines de salchicha"  title="Banderines de salchicha" data-description="Deliciosos banderines de salchicha y queso" />
+                                                <video preload="none" src="https://www.youtube.com/embed/XJRVrJS0xMg?v=XJRVrJS0xMg" ></video>
+                                            </li>
+                                            <li><img src="assets/img/rUs6uLZ_4KM.jpg" alt="Aros de cebolla"  title="Aros de cebolla" data-description="Aros de cebolla super crujientes" />
+                                                <video preload="none" src="https://www.youtube.com/embed/rUs6uLZ_4KM?v=rUs6uLZ_4KM" ></video>
+                                            </li>
+                                        </ul>
+                                        <div class="amazingslider-engine"><a href="http://amazingslider.com" title="jQuery Slideshow">jQuery Slideshow</a></div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+                </div>
+            </div>
         </div>
+    </div>
         <!-- END MAIN CONTENT -->
-    <!-- END MAIN -->
-    <div class="clearfix"></div>
-    <footer>
-        <div class="container-fluid">
-            <p class="copyright">Shared by <i class="fa fa-love"></i><a href="https://bootstrapthemes.co">BootstrapThemes</a>
-            </p>
-        </div>
-    </footer>
-</div>
-<!-- END WRAPPER -->
-<!-- Javascript -->
-<script src="assets/vendor/jquery/jquery.min.js"></script>
-<script src="assets/vendor/bootstrap/js/bootstrap.min.js"></script>
-<script src="assets/vendor/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-<script src="assets/scripts/klorofil-common.js"></script>
+        <!-- END MAIN -->
+        <div class="clearfix"></div>
+        <footer>
+            <div class="container-fluid">
+                <p class="copyright">Shared by <i class="fa fa-love"></i><a href="https://bootstrapthemes.co">BootstrapThemes</a>
+                </p>
+            </div>
+        </footer>
+    </div>
+    <!-- END WRAPPER -->
+    <!-- Javascript -->
+    <script src="assets/vendor/jquery/jquery.min.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/vendor/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+    <script src="assets/scripts/klorofil-common.js"></script>
 </body>
 
 </html>
